52fd32df9f92e686cb881bed2d2d4214ab6a9f15
nikhil soi <aanikhilsoni@gmail.com>
2020-07-02 20:41:08 +0530

    Updates

---
+++ precache-manifest.e6a3dc2fb2c19c4f0e20657e0a2f4c0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90187930e853facfd1b3e68d3ac23c41",
+    "url": "/online-editor/index.html"
+  },
+  {
+    "revision": "890507221b54c4a4b2bd",
+    "url": "/online-editor/static/css/2.986052d2.chunk.css"
+  },
+  {
+    "revision": "cb79504aedb569ece713",
+    "url": "/online-editor/static/css/main.7f3e163c.chunk.css"
+  },
+  {
+    "revision": "890507221b54c4a4b2bd",
+    "url": "/online-editor/static/js/2.b57f0912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-editor/static/js/2.b57f0912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb79504aedb569ece713",
+    "url": "/online-editor/static/js/main.b561a91d.chunk.js"
+  },
+  {
+    "revision": "f9c969f775cad4a4c5ee",
+    "url": "/online-editor/static/js/runtime-main.facbd83f.js"
+  }
+]);
